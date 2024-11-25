4014f665bd3d00edd79e778b295a10f87ddf6ec1
Hossain Al Masud <L00186111@atu.ie>
2024-11-25 20:00:00 +0000

    Update main.tf

--- TerraDemo1/main.tf
+++ TerraDemo1/main.tf
@@ -1,6 +1,9 @@
-provider "local" {
+provider "aws" {
+    region = "eu-west-1"  # Set your desired AWS region
 }
-resource "local_file" "helloworld" {
- content = "Hello ATU student"
+
+resource "aws_instance" "Terraform_Demo1" {
+    ami           = "i-0534f2c54cbb103a8"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
  filename = "hello.txt"
 }
